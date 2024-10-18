9a1bd35bf3cf8db0125d5fbf469749815dd19efa
Erick Quintao <erickquintao@outlook.com>
2024-10-18 17:55:59 -0300

    terminei de criar a entidade Atendimento

--- projeto/projeto/src/main/java/com/gerenciamentoPsicologico/projeto/model/Atendimento.java
+++ projeto/projeto/src/main/java/com/gerenciamentoPsicologico/projeto/model/Atendimento.java
@@ -4,10 +4,136 @@
  */
 package com.gerenciamentoPsicologico.projeto.model;
 
-/**
- *
- * @author erick
- */
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
 public class Atendimento {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "horarioInicial")
+    private LocalDateTime horarioInicial;
+
+    @Column(name = "horarioFinal")
+    private LocalDateTime horarioFinal;
+
+    @Column(name = "valor")
+    private int valor;
+
+    @Column(name = "desconto")
+    private int desconto;
+
+    @Column(name = "valorTotal")
+    private int valorTotal;
+
+    @Column(name = "metodoPagamento")
+    private String metodoPagamento;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "convenio")
+    private String convenio;
+
+    @Column(name = "descricao")
+    private String descricao;
+
+    // Métodos get e set
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDateTime getHorarioInicial() {
+        return horarioInicial;
+    }
+
+    public void setHorarioInicial(LocalDateTime horarioInicial) {
+        this.horarioInicial = horarioInicial;
+    }
+
+    public LocalDateTime getHorarioFinal() {
+        return horarioFinal;
+    }
+
+    public void setHorarioFinal(LocalDateTime horarioFinal) {
+        this.horarioFinal = horarioFinal;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public int getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(int desconto) {
+        this.desconto = desconto;
+    }
+
+    public int getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(int valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public String getMetodoPagamento() {
+        return metodoPagamento;
+    }
+
+    public void setMetodoPagamento(String metodoPagamento) {
+        this.metodoPagamento = metodoPagamento;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getConvenio() {
+        return convenio;
+    }
+
+    public void setConvenio(String convenio) {
+        this.convenio = convenio;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
+
