d7a63f4bb4be4ad278f82c338202c978122e4d3f
Erick Quintao <erickquintao@outlook.com>
2024-10-19 15:13:59 -0300

    configurei main class e crie a base do controller

--- projeto/projeto/src/main/java/com/gerenciamentoPsicologico/projeto/controller/AtendimentoController.java
+++ projeto/projeto/src/main/java/com/gerenciamentoPsicologico/projeto/controller/AtendimentoController.java
@@ -4,10 +4,28 @@
  */
 package com.gerenciamentoPsicologico.projeto.controller;
 
+import com.gerenciamentoPsicologico.projeto.model.Atendimento;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 /**
  *
  * @author erick
  */
 public class AtendimentoController {
-    // eu so preciso preencher aqui quando eu for mexer com db
+    // eu so preciso preencher direito aqui quando eu for mexer com db
+     private List<Atendimento> atendimentos = new ArrayList<>();
+    
+    @GetMapping("/cadastroAtendimento")
+public String mostrarFormularioCadastro(Model model) {
+    model.addAttribute("atendimento", new Atendimento()); // Adiciona um novo objeto Filme ao modelo
+    return "cadastro-filme"; // Nome da página HTML para cadastrar filme
+}
+     @GetMapping("/listarAtendimento")
+    public String listarFilmes(Model model) {
+        model.addAttribute("atendimentos", atendimentos); // Adiciona a lista de filmes ao modelo
+        return "listarAtendimento"; // Nome da página HTML para listar filmes
+    }
 }
